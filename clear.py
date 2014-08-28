dec1847e4340eafb921d22f4d7687fa5eb62ba1c
lordinrho <lordinrho@aims.edu.gh>
2014-08-28 16:15:16 +0000

    new

--- clear.py
+++ clear.py
@@ -1,3 +1,18 @@
 __author__ = 'lord'
 print("We are attempting to perfect our knowledge by having constant practises. However, if it turns to be bad, lets not give up, as long as its not marriage")
 print("Sleeping is an option but giving up might be denial of your capabilities")
+count=3 #declaring an integer variable and assigning
+seek=300.3  #declaring a float variable and assigning
+come="Rho" #declaring a string variable and assigning
+print(count)
+print(seek)
+print(come)
+#new stuffs for practice
+str = 'Hello World!'
+
+print(str)          # Prints complete string
+print(str[0] )      # Prints first character of the string
+print(str[2:5])     # Prints characters starting from 3rd to 5th
+print(str[2:])      # Prints string starting from 3rd character
+print(str * 2)      # Prints string two times
+print(str + "TEST") # Prints concatenated string
