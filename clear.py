b4ab9353fbcd63ae5d80dd87a7a8da437888a296
lordinrho <lordinrho@aims.edu.gh>
2014-08-28 10:06:39 +0000

    Its new addition

---
+++ clear.py
@@ -0,0 +1,3 @@
+__author__ = 'lord'
+print("We are attempting to perfect our knowledge by having constant practises. However, if it turns to be bad, lets not give up, as long as its not marriage")
+print("Sleeping is an option but giving up might be denial of your capabilities")
